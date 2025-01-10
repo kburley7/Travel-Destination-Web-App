52dead76f567b56ed34c32440ac22f7e131c3660
kburley7 <145406170+kburley7@users.noreply.github.com>
2025-01-09 23:26:44 -0500

    Update firebase.js

--- server/firebase.js
+++ server/firebase.js
@@ -3,14 +3,10 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDuy6Ma_f4uHQFiM2N_SyJnD7_k7eE4Mdo",
-  authDomain: "se3316-lab4-936a7.firebaseapp.com",
-  projectId: "se3316-lab4-936a7",
-  storageBucket: "se3316-lab4-936a7.firebasestorage.app",
-  messagingSenderId: "694963918772",
-  appId: "1:694963918772:web:05fcb52986a774ac2184e1"
+ //private firebase config
+
 };
 
 // Initialize Firebase
